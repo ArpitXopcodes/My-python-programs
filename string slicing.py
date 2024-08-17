e0a5d013774db15e9461ab73bdd2727a98c83e23
MOS3S0 <turukmarearpit@gmail.com>
2024-08-17 23:08:10 +0530

    Create string slicing.py

---
+++ string slicing.py
@@ -0,0 +1,15 @@
+'''string slicing'''
+
+name = "arpit"
+nameshort = name[0:3] #start feom index 0 to 2 till 3 thatt means 2''
+print(nameshort)
+charecter1 = name[0]
+print(charecter1)
+
+# negative slicing
+print(name[-3:-1])
+
+#skipping values
+
+game="predator"
+print(game[0:4:2])
