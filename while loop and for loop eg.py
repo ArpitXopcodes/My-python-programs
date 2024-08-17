f0508d182cb6e1087f6513c80fc18cbab206d59d
MOS3S0 <turukmarearpit@gmail.com>
2024-08-17 23:17:29 +0530

    Create while loop and for loop eg.py

---
+++ while loop and for loop eg.py
@@ -0,0 +1,17 @@
+i=1
+
+while(i<6):
+     print("arpit")
+     i+=1
+
+for i in range(0,6,2):
+     print(i)
+
+l=[1,"arpit",False]
+i=0
+while(i<len(l)):
+     print(l[i])
+     i=i+1
+s="akhil"
+for i in s:
+     print(i)
