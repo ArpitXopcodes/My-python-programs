30affee982bf4ae643ea5bd03046250e8234ecfb
MOS3S0 <turukmarearpit@gmail.com>
2024-08-17 23:34:33 +0530

    Create basic methods of byte array.py

---
+++ basic methods of byte array.py
@@ -0,0 +1,19 @@
+# Creating bytearray
+a = bytearray((12, 8, 25, 2))
+print("Creating Bytearray:")
+print(a)
+
+# accessing elements
+print("\nAccessing Elements:", a[1])
+
+# modifying elements
+a[1] = 3
+print("\nAfter Modifying:")
+print(a)
+'''isko hamne chnage kiya
+ mtlb ye changeable hai'''
+
+# Appending elements
+a.append(30)
+print("\nAfter Adding Elements:")
+print(a)
