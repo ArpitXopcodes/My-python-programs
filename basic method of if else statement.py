8408bcfe9bcceee516318e976903d58735dec397
MOS3S0 <turukmarearpit@gmail.com>
2024-08-17 23:15:05 +0530

    Create basic method of if else statement.py

---
+++ basic method of if else statement.py
@@ -0,0 +1,15 @@
+'''basic method if else statement'''
+
+a=int(input("enter your age :"))
+
+
+if(a>=18):
+    print("you are above the age of consent")
+    print("very good for you")
+
+if(a%2 == 0):
+    print("a is even")
+
+
+else:
+    print("you are bellow the age of consent") 
