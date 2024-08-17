501d34e356a2f38003ebde2e92da63665ff6ba51
MOS3S0 <turukmarearpit@gmail.com>
2024-08-17 23:10:15 +0530

    Create functions of list.py

---
+++ functions of list.py
@@ -0,0 +1,17 @@
+'''functions of lists'''
+
+friends = ["apple","orange",5,2.3,False,"akash","akhil"]
+
+print(friends[0])
+
+friends[0]="arpit"
+
+print(friends[0])
+
+print(friends[1:4])
+
+#list can be sliced too
+
+# unlike strings lists are mutable
+
+# this is alist we can change a list which we cant in strings 
