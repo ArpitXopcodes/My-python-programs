38ec31007a48d48972d83b23b0885c5ab70d78a1
MOS3S0 <turukmarearpit@gmail.com>
2024-08-17 23:09:12 +0530

    Create basic methods used in list.py

---
+++ basic methods used in list.py
@@ -0,0 +1,29 @@
+'''basic methods used in list'''
+
+friends = ["apple","orange",5,2.3,False,"akash","akhil"]
+
+print(friends)
+
+friends.append("Harry")
+
+print(friends)
+
+l1 =[1,23,4,25,77,8,56]
+
+l1.sort()
+
+print(l1)
+
+l1.reverse()
+
+print(l1)
+
+l1.insert(0,4)
+
+print(l1)
+
+l1.pop(2)
+
+print(l1)
+
+l1.remove(23)
