e9f090d6d1ef80428a729de8327f68e18d848360
MOS3S0 <turukmarearpit@gmail.com>
2024-08-17 23:12:22 +0530

    Create basic methods of dictionary.py

---
+++ basic methods of dictionary.py
@@ -0,0 +1,23 @@
+'''basic methods of dict'''
+
+marks = {
+"Harry":100,
+
+"shubham":26,
+
+"prassanna":9,
+
+"akhil":96,
+
+"arpit":98
+
+}
+print(type(marks))
+
+print(marks.keys())
+
+print(marks.values())
+
+marks.update({"arpit":100} )
+
+print(marks)
