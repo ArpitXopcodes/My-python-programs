fd7938f7670f7fa13dcb13196acd6cb015efa2e1
MOS3S0 <turukmarearpit@gmail.com>
2024-08-17 23:16:39 +0530

    Create basic methods of for loop.py

---
+++ basic methods of for loop.py
@@ -0,0 +1,12 @@
+'''basic methods of for loops'''
+
+for i in range(1,14):
+     if(i==10):
+         break #exit the loop right now
+     print(i)
+
+
+for i in range(100):
+    if(i==34):
+        continue # skip this itration
+print(i)
